e058d61d4a854b8d1b5e70d86332f0742ebbfdd0
ar2189 <atif2189.ar@gmail.com>
2022-11-06 22:37:36 +0000

    Add code to show the reult of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,4 +75,43 @@ const game = () => {
             }
         }
     }
+
+    const gameOver = (playerOptions, movesLeft) => {
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+
+        chooseMove.innerText = 'Game Over!'
+        movesLeft.style.display = 'none';
+
+        if(playerScore > computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Won The Game';
+            result.style.color = 'gold';
+        }
+        else if(playerScore < computerScore) {
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost The Game';
+            result.style.color = 'red';
+        }
+        else {
+            result.style.fontSize = '2rem';
+            result.innerText = 'Tie';
+            result.style.color = 'grey';
+        }
+
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex';
+        reloadBtn.addEventListener('click', () => {
+            window.location.reload();
+        })
+    }
+
+    playGame();
 }
+
+game();
