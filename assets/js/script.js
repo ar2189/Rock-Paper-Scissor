56b4424d43b62509c881d19632d4e3f27ece7b53
ar2189 <atif2189.ar@gmail.com>
2022-11-06 21:52:51 +0000

    Add function to start playing the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,26 @@ const game = () => {
         const rockBtn = document.querySelector('.rock');
         const paperBtn = document.querySelector('.paper');
         const scissorBtn = document.querySelector('.scissor');
-        const playerOption = [rockBtn, paperBtn, scissorBtn];
-        const computerOption = ['rock', 'paper', 'scissor']
+        const playerOptions = [rockBtn, paperBtn, scissorBtn];
+        const computerOptions = ['rock', 'paper', 'scissor']
+
+
+        playerOptions.forEach(option => {
+            option.addEventListener('click', function() {
+
+                const movesLeft = document.querySelector('.movesleft');
+                moves++;
+                movesLeft.innerText = `Moves Left: ${10-moves}`;
+                
+                const choiceNumber = Math.floor(Math.random()*3);
+                const computerChoice = computerOptions[choiceNumber];
+
+                winner(this.innerText,computerChoice)
+
+                if(moves == 10){
+                    gameOver(playerOptions,movesLeft);
+                }
+            })
+        })
     }
 }
