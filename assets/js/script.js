5b46d5c2ea1963a0859e4bcc105bae29b348359c
ar2189 <atif2189.ar@gmail.com>
2022-11-06 21:35:13 +0000

    Add code for rock, paper and scissor buttons

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+
+
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
+        const playerOption = [rockBtn, paperBtn, scissorBtn];
+        const computerOption = ['rock', 'paper', 'scissor']
+    }
+}
